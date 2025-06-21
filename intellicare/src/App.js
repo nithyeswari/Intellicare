d14677d5246a8beffae4583af39a70192c7aefc0
Nithyeswari <nithyeswari@gmail.com>
2025-06-21 01:34:16 +0100

    Update App.js

--- intellicare/src/App.js
+++ intellicare/src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <IntegratedStrokePredictionSystem></IntegratedStrokePredictionSystem>
+
+<ModernProductPlatform></ModernProductPlatform>
     </div>
   );
 }
